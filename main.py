a16f5ca0cbc3a0732ba681eb425aae833be8d0ad
pdev <pdevfig@gmail.com>
2026-09-17 00:22:48 +0530

    build: add cors

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# tempovarily add all origins
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def entry_point():
