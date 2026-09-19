1931f9548e43896cef9d847ded2356d6998856da
pdev <pdevfig@gmail.com>
2026-09-19 14:03:44 +0530

    build: add datetimeh api endpoint

--- apiv1.py
+++ apiv1.py
@@ -5,5 +5,9 @@ api_v1_router = APIRouter(prefix="/api/v1")
 
 # datetime router will return now
 @api_v1_router.get("/datetime")
-def datetime_delay():
+def datetime_server():
     return {"serverTime": f"{datetime.now()}"}
+
+@api_v1_router.get("/datetimeh")
+def datetime_server_human_readable():
+    return {"Time_human_readable_format": f"{datetime.now().strftime("%A, %B %d, %Y")}"}
